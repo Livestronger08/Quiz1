3fdf8f9b956b22ab29e16f8fbeb6dfe505d8d266
Livestronger08 <derek.downie@ttu.edu>
2020-03-20 03:50:01 +0000

    Quiz 1 Setting up the First Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/travel/show', to: 'travel#show'
-  root :to => "travel#show"
+  root 'travels#index'
 end
